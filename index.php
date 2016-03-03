579723833a87863a90115a01266f9d6cb3305dbc
CDgun <corentindelarbre@gmail.com>
2016-03-03 14:22:09 +0100

    first db acces

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+$dbConfig = parse_ini_file('db.ini');
 
-$cn = new PDO('mysql:dbname=biblio2;host=127.0.0.1','root','');
+try {
+    $dsn = sprintf('%s:dbname=%s;host=%s',$dbConfig['driver'],$dbConfig['dbname'],$dbConfig['host']);
+    $cn = new PDO($dsn, $dbConfig['username'],$dbConfig['password']);
+    $cn->exec('SET CHARACTER SET UTF8');
+    $cn->exec('SET NAMES UTF8');
+} catch (PDOException $exception) {
+    //  redirection vers une page pour afficher une erreur relative à la connexion
+    die($exception->getMessage());
+}
+
+$sqlBooks = 'SELECT * FROM books';
+$pdoSt = $cn->query($sqlBooks);
+$books = $pdoSt->fetchAll();
+var_dump($books);
